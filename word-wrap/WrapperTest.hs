1e36053e7fe58b77065127df8e6848792a81ecb1
Mike Williams <mdub@dogbiscuit.org>
2012-01-01 20:42:58 +1100

    Another test.

--- word-wrap/WrapperTest.hs
+++ word-wrap/WrapperTest.hs
@@ -2,7 +2,10 @@ import Char
 import Test.HUnit
 import Wrapper
 
-tests = TestCase $ assertEqual "not wrapped" "foo" (wrap "foo" 3)
+tests = TestList [
+  TestCase( assertEqual "not wrapped" "foo"          (wrap "foo" 3)         ),
+  TestCase( assertEqual "wrapped"     "foo\nbar"     (wrap "foo bar" 3)     )
+  ]
 
 main = do
   runTestTT tests
